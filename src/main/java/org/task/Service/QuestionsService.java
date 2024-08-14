4d29acf5422335bcf9ed0abedbf79c1b236bdacf
AliBaba1301 <57716311+AliBaba1301@users.noreply.github.com>
2024-08-14 12:37:39 +0100

    Added error handling for validation if question doesn't exist

--- src/main/java/org/task/Service/QuestionsService.java
+++ src/main/java/org/task/Service/QuestionsService.java
@@ -23,17 +23,22 @@ public class QuestionsService {
         return questionsRepository.getAllQuestions(disease);
     }
 
-    public Map<Boolean, List<RejectionResponse>> validateResponses (List<QuestionResponse> responses){
+    public Map<Boolean, List<RejectionResponse>> validateResponses (List<QuestionResponse> responses) throws Exception {
         boolean allValid = true;
         Map<Integer, Map.Entry<Boolean,String>> expectedResponses = questionsRepository.getExpectedResponses();
         Map<Boolean,List<RejectionResponse>> map = new HashMap<>();
         List<RejectionResponse> invalidResponses = new ArrayList<>();
-        for (QuestionResponse response : responses) {
-            if (!(response.getAnswer() == expectedResponses.get(response.getQuestionId()).getKey())){
-                allValid = false;
-                invalidResponses.add(new RejectionResponse(response.getQuestionId(),
-                        expectedResponses.get(response.getQuestionId()).getValue()));
+        try {
+            for (QuestionResponse response : responses) {
+                if (!(response.getAnswer() == expectedResponses.get(response.getQuestionId()).getKey())){
+                    allValid = false;
+                    invalidResponses.add(new RejectionResponse(response.getQuestionId(),
+                            expectedResponses.get(response.getQuestionId()).getValue()));
+                }
             }
+        }
+        catch(Exception e){
+            throw new Exception("Sent a question that does not exist");
         }
         map.put(allValid,invalidResponses);
 
