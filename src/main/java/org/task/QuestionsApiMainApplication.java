46d5a3b29931ad85c8a8b525aad3978bd121b9aa
AliBaba1301 <57716311+AliBaba1301@users.noreply.github.com>
2024-08-14 11:34:05 +0100

    Updated populations statement to reflect task

--- src/main/java/org/task/QuestionsApiMainApplication.java
+++ src/main/java/org/task/QuestionsApiMainApplication.java
@@ -22,19 +22,16 @@ public class QuestionsApiMainApplication {
         String populateTableSql = "INSERT INTO QUESTIONS (Disease, Question, Expected_Answer, Rejection_String) VALUES " +
                 "('generic', 'Are you aged between 18-65?',True,'This treatment is not suitable for you. Please speak to your GP for " +
                 "any further advice')," +
-                "('generic', 'Are you pregnant, breastfeeding or planning a pregnancy?',False,'This treatment is not suitable for you. " +
+                "('generic', 'Are you pregnant, breastfeeding or planning a pregnancy?',False,'This treatment may not suitable for you. " +
                 "Please speak to your GP for any further advice')," +
-                "('arthritis', 'Are you seeking treatment for relief of pain and/or inflammation in a joint or muscle?',True,'This " +
-                "treatment is only suitable for relief of pain and/or inflammation in a joint or muscle. Please speak to your GP for " +
-                "any further advice')," +
-                "('arthritis', 'Have you tried over the counter pain relief options such as paracetamol?',True,'This treatment is " +
-                "for when over the counter painkillers are not strong enough. Please speak to your GP for any further advice')," +
-                "('migraine', 'Do you experience migraines for more than 10 days a month?',False,'This treatment is not suitable for you. " +
-                "Please speak to your GP for any further advice')," +
-                "('migraine', 'Do your migraines follow a broadly similar pattern each time?',True,'This treatment is not suitable for you. " +
-                "Please speak to your GP for any further advice')," +
-                "('migraine', 'Do your migraines last less than 4 hours without treatment or last longer than 24 hours?',False,'This treatment " +
-                "is not suitable for you. Please speak to your GP for any further advice')";
+                "('allergy', 'Do you understand there is a possibility that oral antihistamines may cause drowsiness and affect driving or operating machinery?',True,'Please " +
+                "speak to our helpline for more information')," +
+                "('allergy', 'Have you previously experienced an allergic reaction to antihistamines?',False, " +
+                "'Please speak to your GP about medications that are suitable for you')," +
+                "('allergy', 'Do you understand that you should not take: Antacids or emergency contraception 2 hours before or after taking an antihistamine?'," +
+                "True,'Please speak to to our helpline for more information')," +
+                "('allergy', 'Do you have any history of heart issues?',False,'This treatment is not suitable for you. " +
+                "Please speak to your GP for any further advice')";
 
         ConfigurableApplicationContext ctx = SpringApplication.run(QuestionsApiMainApplication.class, args);
 
