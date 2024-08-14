e700ce71ef74083ad34135925761a934713a7bab
AliBaba1301 <57716311+AliBaba1301@users.noreply.github.com>
2024-08-14 11:17:14 +0100

    Added setup and connection to in memory DB and creation of Questions table

--- src/main/java/org/task/QuestionsApiMainApplication.java
+++ src/main/java/org/task/QuestionsApiMainApplication.java
@@ -1,10 +1,35 @@
 package org.task;
 
-import com.sun.tools.javac.Main;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+@SpringBootApplication
 public class QuestionsApiMainApplication {
-    public static void main(String[] args) {
-            SpringApplication.run(Main.class, args);
+    public static void main(String[] args) throws SQLException {
+        String createSql = "CREATE TABLE Questions ( " +
+                "ID int AUTO_INCREMENT PRIMARY KEY, " +
+                "Disease varchar(255) NOT NULL, " +
+                "Question TEXT NOT NULL, " +
+                "Expected_Answer boolean NOT NULL, " +
+                "Rejection_String TEXT NOT NULL)";
+
+        ConfigurableApplicationContext ctx = SpringApplication.run(QuestionsApiMainApplication.class, args);
+
+        Connection conn = DriverManager.getConnection(
+                ctx.getEnvironment().getProperty("spring.datasource.url"),
+                ctx.getEnvironment().getProperty("spring.datasource.username"),
+                ctx.getEnvironment().getProperty("spring.datasource.password"));
+        conn.setAutoCommit(true);
+
+        Statement configureQuestionsTable =  conn.createStatement();
+        configureQuestionsTable.execute(createSql);
+        conn.close();
         }
-    }
+
+}
